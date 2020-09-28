0c455b63ff449e68d44e32e8d14b5b2efb451422
sonalmahendru <52343997+sonalmahendru@users.noreply.github.com>
2020-09-28 19:43:07 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,6 +18,10 @@ function play_clicked(){
 }
 
 function init_load(){
+    
+    var videosrc = document.getElementByID("prod1").addEventListener('click', onProductClick(0,'prod1')); 
+    var videosrc = document.getElementByID("prod11").addEventListener('click', onProductClick(0,'prod11')); 
+    
     var game_flag = localStorage.getItem("flag");
     var result = localStorage.getItem("result");
     if(game_flag == "2"){
