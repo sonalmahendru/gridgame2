3f74d21b02f352d473b5b574033c813ee28faab2
Sonal Mahendru <Sonal_Mahendru@yahoo.com>
2020-10-11 18:30:49 +0530

    hashtag changed

--- graph.php
+++ graph.php
@@ -5,7 +5,7 @@ if(isset($_POST['imageData'])){
     $imageData = str_replace('data:image/png;base64,', '', $imageData);
     $imageName = time().mt_rand(100000,999999);
     if(file_put_contents("user-results/".$imageName.".png",base64_decode($imageData))){
-        $shareUrl = "https://www.facebook.com/sharer/sharer.php?app_id=826539421486313&display=page&u=https://".$_SERVER['HTTP_HOST']."/graph/".$imageName."&hashtag=%23miamarpooja";
+        $shareUrl = "https://www.facebook.com/sharer/sharer.php?app_id=826539421486313&display=page&u=https://".$_SERVER['HTTP_HOST']."/graph/".$imageName."&hashtag=%23miamarpuja";
         print(json_encode(array("status"=>"200","url"=>$shareUrl)));
     }else{
         print(json_encode(array("status"=>"201")));
